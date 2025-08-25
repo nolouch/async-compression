ba69fdcf9a5071d1678d2bb5ceaa248007d1dd80
nolouch <nolouch@gmail.com>
2025-08-25 15:36:49 -0700

    update
    Signed-off-by: nolouch <nolouch@gmail.com>

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -19,7 +19,7 @@ mod lz4;
 mod lzma;
 #[cfg(feature = "xz")]
 mod xz;
-#[cfg(feature = "lzma")]
+#[cfg(any(feature = "lzma", feature = "xz"))]
 mod xz2;
 #[cfg(feature = "zlib")]
 mod zlib;
@@ -44,7 +44,7 @@ pub(crate) use self::lz4::{Lz4Decoder, Lz4Encoder};
 pub(crate) use self::lzma::{LzmaDecoder, LzmaEncoder};
 #[cfg(feature = "xz")]
 pub(crate) use self::xz::{XzDecoder, XzEncoder};
-#[cfg(feature = "lzma")]
+#[cfg(any(feature = "lzma", feature = "xz"))]
 pub(crate) use self::xz2::{Xz2Decoder, Xz2Encoder, Xz2FileFormat};
 #[cfg(feature = "zlib")]
 pub(crate) use self::zlib::{ZlibDecoder, ZlibEncoder};
